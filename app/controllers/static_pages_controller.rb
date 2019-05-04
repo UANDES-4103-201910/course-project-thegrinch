c6ce58d609173385e764127d7952464158f22ae8
gusvicuna <gpvicuna@miuandes.cl>
2019-05-04 14:36:05 -0400

    footer done header almost done. trying bootstrap

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+	def home
+	end
+
+	def index_categories
+	end
+
+	def about
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,34 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :comments
   resources :posts
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'start_page#home'
+
+  get '/home', to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/register', to: 'users#new'
+  post '/register', to: 'users#create'
+  get '/users/:id/settings', to: 'users#edit'
+
+  get '/projects', to: 'projects#index'
+  delete '/project/:id', to: 'projects#destroy'
+
+  get '/categories', to: 'pages#index_categories'
+  post '/categories', to: 'pages#create_category'
+
+  get '/projects/:id/fund', to: 'funds#new'
+  post '/projects/:id/fund', to: 'funds#create'
+
+  # post '/users/:id/follow', to: 'users#follow'
+  # post '/projects/:id/favorite', to: 'projects#favorite'
+
+  post '/users/:id/projects/new', to: 'projects#create'
+  post '/projects/:id/promises/new', to: 'promises#create'
+
+  # get    '/login',   to: 'sessions#new'
+  # post   '/login',   to: 'sessions#create'
+  # delete '/logout',  to: 'sessions#destroy'
+  root 'static_pages#home'
+  root 'static_pages#home'
 end
